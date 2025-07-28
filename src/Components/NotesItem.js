a271be24cadfcdebbe019c189ffaed79b0c5babf
Yuvraj Prakashh <yuvrajprakash4321@gmail.com>
2025-07-28 22:56:59 +0530

    minor fix

--- src/Components/NotesItem.js
+++ src/Components/NotesItem.js
@@ -13,7 +13,18 @@ const NotesItem = (props) => {
           <h5 className="card-title">{note.title}</h5>
           <p className="card-text">{note.description}</p>
            <i className="fa-solid fa-trash mx-2" onClick={()=>{deleteNote(note._id)}}></i>
-           <i className="fa-solid fa-pen-to-square mx-2" onClick={()=>{editNote(note)}}></i>
+           <i
+  className="fa-solid fa-pen-to-square mx-2"
+  onClick={() => {
+    const newTitle = prompt("Edit Title", note.title);
+    const newDescription = prompt("Edit Description", note.description);
+    const newTag = prompt("Edit Tag", note.tag);
+    if (newTitle && newDescription) {
+      editNote(note._id, newTitle, newDescription, newTag);
+    }
+  }}
+></i>
+
         </div>
       </div>
     </div>
